d2d6d49c1d30232ecfe9fd773c597aedf42f8734
Ken Fyfe <kenfyfe@kenfyfe.com>
2020-02-06 15:19:45 +0000

    using parameterized queries to prevent SQL injection; TODO: turn off connection after complete

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -7,15 +7,11 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-const [ cohortName, limitNumber ] = process.argv.slice(2);
+const [cohortName, limitNumber] = process.argv.slice(2);
 
-pool.query(`
-SELECT students.id as student_id, students.name as name, cohorts.name as cohort
-FROM students
-JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${cohortName}%'
-LIMIT ${limitNumber || 5};
-`)
+pool.query('SELECT students.id as student_id, students.name as name, cohorts.name as cohort ' +
+           'FROM students JOIN cohorts ON cohorts.id = cohort_id WHERE cohorts.name LIKE $1 LIMIT $2;',
+           [cohortName, limitNumber])
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,21 +7,14 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-const [ cohortName ] = process.argv.slice(2);
+const [cohortName] = process.argv.slice(2);
 
-pool.query(`
-SELECT
-  DISTINCT(teachers.name) AS teacher,
-  cohorts.name AS cohort
-FROM teachers
-JOIN assistance_requests ON teachers.id = teacher_id
-JOIN students ON students.id = student_id
-JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name = '${cohortName}'
-ORDER BY teacher;
-`)
+pool.query('SELECT DISTINCT(teachers.name) AS teacher, cohorts.name AS cohort FROM teachers ' +
+           'JOIN assistance_requests ON teachers.id = teacher_id JOIN students ON students.id = student_id ' +
+           'JOIN cohorts ON cohorts.id = cohort_id WHERE cohorts.name = $1 ORDER BY teacher;', [cohortName])
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.cohort}: ${user.teacher}`);
   });
+
 }).catch(err => console.error('query error', err.stack));
